547e95da3d3ca7df864f708454f379255c614e0f
jasonwer <jasonwer@Argos.Argos>
2024-02-26 23:50:43 +1000

    first commit

---
+++ hello/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+int ln =0;
+
+puts("hello world.");
+ln = strlen("jfjjhggh");
+printf("%d",ln);
+
+return EXIT_SUCCESS;
+}
+
